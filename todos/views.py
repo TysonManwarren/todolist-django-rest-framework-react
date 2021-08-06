bfac8fc25c3fc3bf0e1588fe8bb24cb7c25ef4d7
Tyson Manwarren <tmanwarren@empirenatgas.com>
2021-08-06 16:07:32 -0400

    Browsable API

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,17 @@
+# todos/views.py
+from django.db.models import query
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Todo
+from .serializers import TodoSerializer
+
+
+class ListTodo(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class DetailTodo(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
